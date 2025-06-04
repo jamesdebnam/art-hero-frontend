384e8687d96e0ade51de94d0dfd1124c80bca80d
James Debnam <jamesdebnam@perlego.com>
2025-06-04 12:41:19 +0100

    pixel grid!!

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,8 @@ import Browser
 import Dict exposing (Dict)
 import Debug
 import Html exposing (Html, button, div, h1, text)
+import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 
 
@@ -39,6 +41,35 @@ update msg model =
             {model | pixelMap = Dict.insert pixelCoords color model.pixelMap}
 
 
+--get_color: Color -> String
+--get_color color =
+--    case color of
+--        Red -> '#ff5733'
+--
+
+
+view_pixel_grid: Model -> Html Msg
+view_pixel_grid model = div [
+    style "display" "flex",
+    style "flex-direction" "column"
+    ]  (List.indexedMap (\row items ->
+                                    div [
+                                            style "display" "flex",
+                                            style "flex-direction" "row"
+                                    ] (
+                                        List.indexedMap (\col _ ->
+                                            div
+                                                [ style "background-color" "white"
+                                                , style "border" "1px solid black"
+                                                , style "height" "30px"
+                                                , style "width" "30px"
+                                                , onClick (PaintPixel model.activeColor (col, row))
+                                                ]
+                                                []
+                                            )  items
+                                        ) ) (List.repeat 6 (List.repeat 6 0)))
+
+
 
 
 
@@ -47,5 +78,6 @@ view model =
   div []
     [
       h1 [ onClick (PaintPixel Blue (0,0)) ] [ text "this is a page"],
-      h1 [ onClick (PaintPixel Red (1,1)) ] [ text "this is also a page"]
+      h1 [ onClick (PaintPixel Red (1,1)) ] [ text "this is also a page"],
+      view_pixel_grid model
     ]

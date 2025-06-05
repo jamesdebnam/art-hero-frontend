1174240bbd8338d398b505130a06d58be087f2f4
RalphField <ralph@perlego.com>
2025-06-05 10:57:38 +0100

    made the bloody API request work finally :')

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,29 @@
 module Main exposing (..)
 
 import Browser
+import Browser.Navigation
 import Dict exposing (Dict)
 import Html exposing (Html, button, div, h1, p, text)
 import Html.Attributes exposing (class, style)
 import Html.Events exposing (onClick, onMouseDown, onMouseOver, onMouseUp)
+import Http
+import Json.Decode as D
+import Url
 
 
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.application
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        , onUrlChange =
+            \_ ->
+                None
+        , onUrlRequest =
+            \_ ->
+                None
+        }
 
 
 maxHistorySize : Int
@@ -42,23 +57,42 @@ type alias History =
     List PixelMap
 
 
+type FetchState
+    = Loading
+    | Success MasterpieceList
+    | Error
+
+
 type alias Model =
     { mouseIsDown : Bool
     , activeColor : Color
     , pixelMap : PixelMap
     , history : History
     , future : History
+    , fetchState : FetchState
     }
 
 
-init : Model
-init =
-    { mouseIsDown = False, activeColor = Red, pixelMap = Dict.empty, history = [ Dict.empty ], future = [ Dict.empty ] }
+init : Bool -> Url.Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
+init _ _ _ =
+    ( { mouseIsDown = False
+      , activeColor = Red
+      , pixelMap = Dict.empty
+      , history = [ Dict.empty ]
+      , future = [ Dict.empty ]
+      , fetchState = Loading
+      }
+    , getMasterpieces
+    )
 
 
 type MouseAction
     = Clicked
     | MouseOver
+
+
+type alias FetchedMasterpieces =
+    List MasterpieceListItem
 
 
 type Msg
@@ -67,21 +101,28 @@ type Msg
     | PaintPixel Color PixelCoords MouseAction
     | Undo
     | Redo
+    | GotMasterpieces (Result Http.Error FetchedMasterpieces)
+    | None
 
 
-update : Msg -> Model -> Model
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         UpdateMouseDown mouseIsDown ->
             case mouseIsDown of
                 True ->
-                    { model | mouseIsDown = mouseIsDown }
+                    ({ model | mouseIsDown = mouseIsDown }, Cmd.none)
 
                 False ->
-                    { model | mouseIsDown = mouseIsDown, history = model.pixelMap :: model.history }
+                    ({ model | mouseIsDown = mouseIsDown, history = model.pixelMap :: model.history }, Cmd.none)
 
         UpdateActiveColor color ->
-            { model | activeColor = color }
+            ({ model | activeColor = color }, Cmd.none)
 
         PaintPixel color pixelCoords mouseAction ->
             case ( mouseAction, model.mouseIsDown ) of
@@ -93,24 +134,24 @@ update msg model =
                         newHistory =
                             model.pixelMap :: model.history
                     in
-                    { model
-                        | pixelMap = newPixelMap
-                        , history = List.take maxHistorySize newHistory
-                        , future = []
-                    }
+                      ({ model
+                          | pixelMap = newPixelMap
+                          , history = List.take maxHistorySize newHistory
+                          , future = []
+                      }, Cmd.none)
 
                 ( MouseOver, True ) ->
                     let
                         newPixelMap =
                             Dict.insert pixelCoords color model.pixelMap
                     in
-                    { model
-                        | pixelMap = newPixelMap
-                        , future = []
-                    }
+                      ({ model
+                          | pixelMap = newPixelMap
+                          , future = []
+                      }, Cmd.none)
 
                 ( MouseOver, False ) ->
-                    model
+                    (model, Cmd.none)
 
         Undo ->
             case model.history of
@@ -119,14 +160,14 @@ update msg model =
                         newFuture =
                             currentState :: model.future
                     in
-                    { model
-                        | pixelMap = previousState
-                        , history = previousState :: restOfTheList
-                        , future = List.take maxFutureSize newFuture
-                    }
+                      ({ model
+                          | pixelMap = previousState
+                          , history = previousState :: restOfTheList
+                          , future = List.take maxFutureSize newFuture
+                      }, Cmd.none)
 
                 _ ->
-                    model
+                    (model, Cmd.none)
 
         Redo ->
             case model.future of
@@ -135,14 +176,24 @@ update msg model =
                         newHistory =
                             futureState :: model.history
                     in
-                    { model
-                        | pixelMap = futureState
-                        , future = restOfTheList
-                        , history = List.take maxHistorySize newHistory
-                    }
+                      ({ model
+                          | pixelMap = futureState
+                          , future = restOfTheList
+                          , history = List.take maxHistorySize newHistory
+                      }, Cmd.none)
 
                 _ ->
-                    model
+                    (model, Cmd.none)
+
+        GotMasterpieces result ->
+            case result of
+                Ok jsonData ->
+                    ({ model | fetchState = Success jsonData }, Cmd.none)
+
+                Err _ ->
+                    ({ model | fetchState = Error }, Cmd.none)
+        None ->
+          (model, Cmd.none)
 
 
 get_pixel_color_from_coords : PixelCoords -> Model -> String
@@ -230,39 +281,45 @@ view_color_button model =
         )
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    div [ class "container" ]
-        [ h1 [] [ text "ART HERO!!!!" ]
-        , div [ class "row-bottom" ]
-            [ view_color_button model
-            , div [ class "column" ]
-                [ div [class "button-row"] [ button
+    { title = "Fuckin elm"
+    , body =
+        [ div [ class "container" ]
+            [ h1 [] [ text "ART HERO!!!!" ]
+            , div [ class "row-bottom" ]
+                [ view_color_button model
+                , div [ class "column" ]
+                    [ div [ class "button-row" ]
+                        [ button
+                            [ onClick Undo
+                            , class
+                                (if List.length model.history > 0 then
+                                    "button"
 
-                    [ onClick Undo
-                    , class
-                        (if List.length model.history > 0 then
-                            "button"
+                                 else
+                                    "button button-disabled"
+                                )
+                            ]
+                            [ text "Undo" ]
+                        , button
+                            [ onClick Redo
+                            , class
+                                (if List.length model.future > 0 then
+                                    "button"
 
-                         else
-                            "button button-disabled"
-                        )
+                                 else
+                                    "button button-disabled"
+                                )
+                            ]
+                            [ text "Redo" ]
+                        ]
+                    , view_pixel_grid model
                     ]
-                    [ text "Undo" ]
-                , button
-                    [ onClick Redo
-                    , class
-                        (if List.length model.future > 0 then
-                            "button"
-                         else
-                            "button button-disabled"
-                        )
-                    ]
-                    [ text "Redo" ]]
-                , view_pixel_grid model
                 ]
             ]
         ]
+    }
 
 
 colorToString : Color -> String
@@ -286,43 +343,33 @@ colorToString color =
         White ->
             "White"
 
+
+
 -- API DECODING SHIT --
 
-type alias MasterpieceData = Dict PixelCoords Color
 
-type alias MasterpieceList = List MasterpieceListItem
+type alias MasterpieceData =
+    Dict PixelCoords Color
 
-type alias MasterpieceListItem = {
-    id: Int,
-    data: MasterpieceData,
-    created_at: String
-  }
 
-type Model
-  = Failure
-  | Loading
-  | Success MasterpieceList
+type alias MasterpieceList =
+    List MasterpieceListItem
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = 
-  case msg of
-    FetchMasterpieces ->
-      (Loading, getMasterpieces)
 
-    GotMasterpieces result ->
-      case result of
-        Ok jsonData ->
-          (Success jsonData, Cmd.none)
-        Err _ ->
-          (Failure, Cmd.none)
+type alias MasterpieceListItem =
+    { id : Int
+    , data : MasterpieceData
+    , created_at : String
+    }
+
 
 getMasterpieces : Cmd Msg
 getMasterpieces =
-  Http.get
-    {
-      url = "http://localhost:3000/masterpiece",
-      expect = Http.expectJson GotMasterpieces (D.list masterpieceListItemDecoder)
-    }
+    Http.get
+        { url = "http://localhost:3000/masterpiece"
+        , expect = Http.expectJson GotMasterpieces (D.list masterpieceListItemDecoder)
+        }
+
 
 colourDecoder : D.Decoder Color
 colourDecoder =
@@ -330,37 +377,54 @@ colourDecoder =
         |> D.andThen
             (\str ->
                 case str of
-                    "Red" -> D.succeed Red
-                    "Black" -> D.succeed Black
-                    "Blue" -> D.succeed Blue
-                    "Green" -> D.succeed Green
-                    "Yellow" -> D.succeed Yellow
-                    "White" -> D.succeed White
-                    _ -> D.fail ("Unknown colour: " ++ str)
+                    "Red" ->
+                        D.succeed Red
+
+                    "Black" ->
+                        D.succeed Black
+
+                    "Blue" ->
+                        D.succeed Blue
+
+                    "Green" ->
+                        D.succeed Green
+
+                    "Yellow" ->
+                        D.succeed Yellow
+
+                    "White" ->
+                        D.succeed White
+
+                    _ ->
+                        D.fail ("Unknown colour: " ++ str)
             )
+
 
 coordsFromString : String -> Result String PixelCoords
 coordsFromString str =
     let
         trimmed =
             String.trim str
-                |> String.dropLeft 1  -- remove '['
-                |> String.dropRight 1 -- remove ']'
+                |> String.dropLeft 1
+                -- remove '['
+                |> String.dropRight 1
 
+        -- remove ']'
         parts =
             String.split "," trimmed
     in
     case parts of
-        [xStr, yStr] ->
-            case (String.toInt xStr, String.toInt yStr) of
-                (Just x, Just y) ->
-                    Ok (x, y)
+        [ xStr, yStr ] ->
+            case ( String.toInt xStr, String.toInt yStr ) of
+                ( Just x, Just y ) ->
+                    Ok ( x, y )
 
                 _ ->
                     Err ("Invalid coordinate numbers in: " ++ str)
 
         _ ->
             Err ("Invalid coordinate format: " ++ str)
+
 
 masterpieceDataDecoder : D.Decoder MasterpieceData
 masterpieceDataDecoder =
@@ -370,22 +434,24 @@ masterpieceDataDecoder =
                 let
                     resultList =
                         Dict.toList rawDict
-                            |> List.map (\(k, v) -> Result.map (\coords -> (coords, v)) (coordsFromString k))
+                            |> List.map (\( k, v ) -> Result.map (\coords -> ( coords, v )) (coordsFromString k))
                 in
-                case List.foldr
+                case
+                    List.foldr
                         (\res acc ->
-                            case (res, acc) of
-                                (Ok (coords, colour), Ok dict) ->
+                            case ( res, acc ) of
+                                ( Ok ( coords, colour ), Ok dict ) ->
                                     Ok (Dict.insert coords colour dict)
 
-                                (Err e, _) ->
+                                ( Err e, _ ) ->
                                     Err e
 
-                                (_, Err e) ->
+                                ( _, Err e ) ->
                                     Err e
                         )
                         (Ok Dict.empty)
-                        resultList of
+                        resultList
+                of
                     Ok finalDict ->
                         D.succeed finalDict
 
@@ -393,11 +459,14 @@ masterpieceDataDecoder =
                         D.fail msg
             )
 
+
 masterpieceListItemDecoder : D.Decoder MasterpieceListItem
 masterpieceListItemDecoder =
     D.map3 MasterpieceListItem
         (D.field "id" D.int)
         (D.field "data" masterpieceDataDecoder)
         (D.field "created_at" D.string)
+
+
 
 -- API DECODING SHIT END --
